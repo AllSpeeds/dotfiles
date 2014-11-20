1506f7b0ff1e8107e40fb03d4fef94afcfaf02fe
Chad Gowan <cgowan@spredfast.com>
2014-11-20 16:27:00 -0600

    - Adding .aliases with SpotifyControl - Adding .brew with update, upgrade, install: tree & phantomjs, cleanup - Adding exports with history changes - Adding install-deps.sh with homebrew and SpotifyControl - Using single # for comments in sass-lint.yml and .bashrc

---
+++ install-deps.sh
@@ -0,0 +1,6 @@
+# homebrew!
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# SpotifyControl
+cd ~/src
+git clone git://github.com/dronir/SpotifyControl.git
