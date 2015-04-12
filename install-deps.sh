b0e889dd693556c543ed2bc4c90805e7e53601c0
Chad Gowan <cgowan@spredfast.com>
2015-04-11 22:44:22 -0500

    Installing deps, node modules and gems

--- install-deps.sh
+++ install-deps.sh
@@ -10,6 +10,15 @@ git clone git://github.com/dronir/SpotifyControl.git
 # use 'subl'
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 
+# commonly used npm deps
+npm install -g bower grunt-cli grunt-devtools phantomjs jshint jscs node-inspector pageres
+npm list -g --depth=0
+
+# common gems
+gem install sass compass compass-css-arrow compass-import-once scss-lint
+gem install --pre sass-css-importer
+gem list
+
 # TODO:
 # git-completion.sh
 # heroku
